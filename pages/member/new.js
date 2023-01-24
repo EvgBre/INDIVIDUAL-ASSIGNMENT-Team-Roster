6ffb53156b30c03d7b010f39dc3e1b77fce9b931
Evan Breland <evan.breland@gmail.com>
2023-01-23 21:14:52 -0600

    added member files and formto the wrong branch

---
+++ pages/member/new.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import MemberForm from '../../components/forms/MemberForm';
+
+// Reuseable for to add/edit member in this view
+
+export default function AddMember() {
+  return <MemberForm />;
+}
